9b9c1a8a806bfd88d3ef4aa1bfe24165c0711859
mdakram09 <iamakramkhan09@gmail.com>
2020-08-23 12:47:45 +0530

    first commit : connected to django, login signup modal added, all pages are connected by template inheritance

---
+++ home/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path, include
+from home import views
+from . import views
+
+urlpatterns = [ 
+   path('', views.home, name='home'),
+   path('about', views.about, name='about'),
+   path('contact', views.contact, name="contact"),
+   path('services', views.services, name="contact"),
+   path('faq', views.faq, name="contact"),
+   path('team', views.team, name="contact"),
+]
